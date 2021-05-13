b868fa1e81f897892b1ac96aae3a0c688cfd9965
Michael Corrieri <mlcorrieri@gmail.com>
2021-05-12 21:23:13 -0400

    first fetch done on National Wed 930PM

--- src/National.js
+++ src/National.js
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react';
 import StateCard from './StateCard'
 
 function National() {
-    
-  // fetch all data here 
+ const [statesArr, setStatesArr] = useState([])
+  
+
+  useEffect(() => {
+    fetch("http://localhost:9292/states")
+        .then(resp => resp.json())
+        .then(function(statesData) {
+          setStatesArr(statesData)
+        })
+  }, [])
   
   
   
   
   
-  return <h1>National</h1>
-  
-    
+  return (
+    <div>
+      <h1>National</h1>
+        <div>
+          <p>{statesArr}</p>
+        </div>
+    </div>
+  ) 
   }
   
   export default National;
